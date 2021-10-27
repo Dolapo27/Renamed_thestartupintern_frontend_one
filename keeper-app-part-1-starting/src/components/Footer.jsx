63db7193f170cf8c96106561e83b609d74ff4b08
Oladipupo Dolapo <oladipupodolapo27@gmail.com>
2021-10-27 09:08:09 +0100

    updated new files

---
+++ keeper-app-part-1-starting/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+
+function Footer(){
+    
+  const date = new Date()
+  const currentDate = date.getFullYear()
+  return (
+      <footer>
+        <p>Copyright{currentDate}</p>
+      </footer>
+  )
+}
+
+export default Footer;
